9b060bfaaeb9925a76016930970bd12746956d64
dgunda <dgunda@walmartlabs.om>
2018-01-14 11:55:56 -0800

    list1

---
+++ src/main/java/linkedlist/List1.java
@@ -0,0 +1,31 @@
+package linkedlist;
+
+public class List1 {
+    public Node head;
+   class Node{
+       private int value;
+       private Node next;
+
+       public Node(int value){
+           this.value = value;
+       }
+   }
+
+   public void add(int value){
+       if(head == null){
+           head = new Node(value);
+           head.next = null;
+       }else{
+           Node n = new Node(value);
+           n.next = null;
+           head.next = n;
+       }
+   }
+   public void printList(){
+       Node temp = head;
+       while(temp!=null){
+           System.out.println(temp.value);
+           temp = temp.next;
+       }
+   }
+}
