8f20bace9b0602f518787c178ed15cce8ae16d03
dgunda <dgunda@walmartlabs.om>
2018-01-25 14:19:34 -0800

    longestplaindromesubstring

---
+++ src/main/java/hr/LogestPalindromeSubString.java
@@ -0,0 +1,31 @@
+package hr;
+
+public class LogestPalindromeSubString {
+    public static void main(String[] args) {
+        String test ="abacdgfdcaabaa";
+        System.out.println(logestPalindromSubstring(test));
+
+    }
+    public static String logestPalindromSubstring(String s){
+        int start=0, end=0;
+        for(int i = 0; i < s.length(); i++){
+            int len1= expandAroundTheCenter(s, i, i);
+            int len2= expandAroundTheCenter(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if(len > end-start){
+                start = i-(len-1)/2;
+                end = i+(len)/2;
+            }
+        }
+        return s.substring(start+1, end);
+
+    }
+    public static int expandAroundTheCenter(String s, int left, int right){
+        int L = left, R = right;
+        while (L>=0 && R<s.length() && s.charAt(L) == s.charAt(R)){
+            L--;
+            R++;
+        }
+        return R-L+1;
+    }
+}
